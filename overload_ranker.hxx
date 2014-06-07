18886ec9d52129ffee0af6ef9788479a73df7797
Robert Haberlach <R.HL@gmx.net>
2014-06-07 12:08:15 +0100

    Added overload resolution ranking system

---
+++ overload_ranker.hxx
@@ -0,0 +1,38 @@
+#ifndef OVERLOAD_RANKER_HXX_INCLUDED
+#define OVERLOAD_RANKER_HXX_INCLUDED
+
+#include "typedefs.hxx"
+/** If you want to gain independence from this header, define index_type in the namespace vtmpl
+    as a (preferably) unsigned integer type. */
+
+namespace vtmpl
+{
+
+#ifdef VTMPL_OVERLOAD_RANK_LIMIT
+	constexpr index_type overload_rank_limit = VTMPL_OVERLOAD_RANK_LIMIT;
+#else
+	constexpr index_type overload_rank_limit = 10;
+#endif
+
+	template< index_type i >
+	struct rank : rank<i + 1> {};
+
+	template<>
+	struct rank<overload_rank_limit> {};
+
+	using first_choice   = rank<0>;
+	using second_choice  = rank<1>;
+	using third_choice   = rank<2>;
+	using fourth_choice  = rank<3>;
+	using fifth_choice   = rank<4>;
+	using sixth_choice   = rank<5>;
+	using seventh_choice = rank<6>;
+	using eigth_choice   = rank<7>;
+	using ninth_choice   = rank<8>;
+	using tenth_choice   = rank<9>;
+
+	using default_choice = rank<overload_rank_limit>;
+	rank<0> ranked_call;
+}
+
+#endif // OVERLOAD_RANKER_HXX_INCLUDED
