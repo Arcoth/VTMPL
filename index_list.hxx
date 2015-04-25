2bf69a01a64c2689af39ea43a01bb78ea363547a
Arcoth <R.HL@gmx.net>
2015-04-25 14:28:23 +0100

    Update index_list.hxx

--- index_list.hxx
+++ index_list.hxx
@@ -1,4 +1,4 @@
-/* Copyright (c) Arcoth@c-plusplus.net, 2013-2014.
+/* Copyright (c) Arcoth, 2013-2015.
 
 	Distributed under the Boost Software License, Version 1.0. (See accompanying
 	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
